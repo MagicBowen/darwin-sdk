2478e6a429fe9dcbbe810ba0bd16e6fd62e683bc
magicbowen <e.bowen.wang@icloud.com>
2018-11-27 17:18:43 +0800

    setup project

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Darwin NodeJs SDK
+
+[NodeJS SDK of Darwin Conversational AI Platform](http://www.xiaoda.ai).
---
+++ src/chatbot.js
@@ -0,0 +1,48 @@
+const postJson = require('./post-json');
+const debug   = require('debug')('darwin:Chatbot');
+
+class Chatbot {
+    constructor(agent, uri) {
+        this.agent = agent;
+        this.uri = uri;
+    }
+    
+    async replyToText(userId, text, userContext) {
+        let data = { query   : { query : text, confidence : 1.0 }, 
+                     session : userId, 
+                     agent   : this.agent,
+                     userContext : userContext };
+
+        let response = await postJson(this.uri, data);
+        return this.formatResponse(response);
+    }
+
+    async replyToEvent(userId, eventType, userContext, params) {
+        let data = { event   : { name : eventType, content : params },
+                     session : userId, 
+                     agent   : this.agent,
+                     userContext : userContext};
+
+        let response = await postJson(this.uri, data);
+        return this.formatResponse(response);
+    }
+
+    formatResponse(response) {
+        debug(`chatbot reply ${JSON.stringify(response)}`);
+        if (response.reply) {
+            let result = this.concatReplies(response.reply);
+            response.reply = result
+        }
+        return response;
+    }
+
+    concatReplies(replies) {
+        var result = '';
+        for(var i = 0; i < replies.length; i++) {
+            result += replies[i];
+        }
+        return result;
+    }
+}
+
+module.exports = Chatbot;
